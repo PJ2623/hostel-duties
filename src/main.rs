349efc21b06f72016a98d920f0d0fa9c8e8d3703
Patrick Mateus <patrick.mateus@adaire.com>
2026-07-23 01:11:19 +0200

    chore: initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+struct Learner {
+    first_name: String,
+    last_name: String,
+    block: char,
+    room: char,
+}
+
+struct Duty {
+    title: String,
+    total_assignees: i32,
+}
+
+fn get_learner_details() {
+    // Prompt for learner details in cmd
+}
+
+fn get_duty_details() {
+    // Prompt for duty details in cmd
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
